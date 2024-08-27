89449f867252b3a471423b7833e0ee320dec89cc
striker561 <itzstriker561@gmail.com>
2024-08-27 20:15:04 +0100

    @update/ Updated the save module

--- modules/save.py
+++ modules/save.py
@@ -1,8 +1,11 @@
 import json
+import os
 
 def save_to_json(data, file_name='data.json'):
-    #SAVE THE DATA TO JSON FILE
-    file_name = 'data/' + file_name
+    os.makedirs('data', exist_ok=True)
+    file_name = os.path.join('data', file_name)
+
     with open(file_name, 'w', encoding='utf-8') as json_file:
-        json.dump(data, json_file, ensure_ascii=False, indent=2)
+        json.dump(data, json_file, ensure_ascii=False, indent=2,)
+        
     print(f"Data scraped and saved to '{file_name}'.")
